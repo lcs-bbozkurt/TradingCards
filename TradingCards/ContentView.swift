586a7d455c4204c548dd8b5dcefb7919698cdb2e
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-11-22 14:32:38 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Berk Bozkurt on 2021-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Berk Bozkurt on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
